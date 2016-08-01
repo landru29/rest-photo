8c237965d3b24205cae96c51ddd367094d6571b4
Cyrille Meichel <cyrille.meichel@corp.ovh.com>
2016-08-01 13:42:14 +0200

    default limit in paginator

--- application/Middleware/Paginator.php
+++ application/Middleware/Paginator.php
@@ -15,7 +15,7 @@ class Middleware_Paginator {
 
     function middleware ($req, $res) {
         $page = array_key_exists('page', $req->query) ? $req->query['page'] : 1;
-        $limit = array_key_exists('limit', $req->query) ? $req->query['limit'] : 10;
+        $limit = array_key_exists('limit', $req->query) ? $req->query['limit'] : $defaultLimit;
         $limit = array_key_exists('perPage', $req->query) ? $req->query['perPage'] : $limit;
         $offset = array_key_exists('offset', $req->query) ? $req->query['offset'] : ($page - 1) * $limit;
         $req->pagination = array(
